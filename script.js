16727402a9225f85a609478e07a31785cac7865c
John Walling <j00walling@gmail.com>
2021-05-15 16:03:24 -0700

    huge biographies update im happy

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-var modal = document.getElementById("myModal");
+function myFunc(myModal, myImg, myModalImg, imgSrc) {
+  var modal = document.getElementById(myModal);
+  var img = document.getElementById(myImg);
+  var modalImg = document.getElementById(myModalImg);
 
-var img = document.getElementById("imgID");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
   modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  modalImg.src = imgSrc;
   document.body.style.overflow = "hidden";
   document.body.style.height = "100%";
+}
+
+var danson = document.getElementById("dansonModal");
+var ian = document.getElementById("ianModal");
+var john = document.getElementById("johnModal");
+var long = document.getElementById("longModal");
+var riya = document.getElementById("riyaModal");
+var sharon = document.getElementById("sharonModal");
+var shlok = document.getElementById("shlokModal");
+var span0 = document.getElementsByClassName("close")[0];
+var span1 = document.getElementsByClassName("close")[1];
+var span2 = document.getElementsByClassName("close")[2];
+var span3 = document.getElementsByClassName("close")[3];
+var span4 = document.getElementsByClassName("close")[4];
+var span5 = document.getElementsByClassName("close")[5];
+var span6 = document.getElementsByClassName("close")[6];
+
+span0.onclick = function () {
+  danson.style.display = "none";
 };
 
-var span = document.getElementsByClassName("close")[0];
+span1.onclick = function () {
+  ian.style.display = "none";
+};
 
-span.onclick = function () {
+span2.onclick = function () {
+  john.style.display = "none";
+};
+
+span3.onclick = function () {
+  long.style.display = "none";
+};
+
+span4.onclick = function () {
+  riya.style.display = "none";
+};
+
+span5.onclick = function () {
+  sharon.style.display = "none";
+};
+
+span6.onclick = function () {
+  shlok.style.display = "none";
+};
+
+function close(myModal) {
+  var modal = document.getElementById(myModal);
   modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+}
