b5898cd64a33ada6aee8efee1530581e764b33c5
John Walling <j00walling@gmail.com>
2021-05-15 19:13:43 -0700

    finished biographies

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function myFunc(myModal, myImg, myModalImg, imgSrc) {
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
+      document.body.style.overflow = "visible";
+      document.body.style.height = "100%";
     }
   };
 
@@ -18,13 +20,17 @@ function myFunc(myModal, myImg, myModalImg, imgSrc) {
 var danson = document.getElementById("dansonModal");
 var ian = document.getElementById("ianModal");
 var john = document.getElementById("johnModal");
+var katherine = document.getElementById("katherineModal");
+var kelly = document.getElementById("kellyModal");
+var lee = document.getElementById("leeModal");
 var long = document.getElementById("longModal");
+var natalie = document.getElementById("natalieModal");
+var nicole = document.getElementById("nicoleModal");
 var riya = document.getElementById("riyaModal");
 var sharon = document.getElementById("sharonModal");
 var shlok = document.getElementById("shlokModal");
 var zilong = document.getElementById("zilongModal");
-var lee = document.getElementById("leeModal");
-var kelly = document.getElementById("kellyModal");
+
 var span0 = document.getElementsByClassName("close")[0];
 var span1 = document.getElementsByClassName("close")[1];
 var span2 = document.getElementsByClassName("close")[2];
@@ -35,48 +41,91 @@ var span6 = document.getElementsByClassName("close")[6];
 var span7 = document.getElementsByClassName("close")[7];
 var span8 = document.getElementsByClassName("close")[8];
 var span9 = document.getElementsByClassName("close")[9];
+var span10 = document.getElementsByClassName("close")[10];
+var span11 = document.getElementsByClassName("close")[11];
+var span12 = document.getElementsByClassName("close")[12];
 
 span0.onclick = function () {
   danson.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span1.onclick = function () {
   ian.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span2.onclick = function () {
   john.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span3.onclick = function () {
-  long.style.display = "none";
+  katherine.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span4.onclick = function () {
-  riya.style.display = "none";
+  kelly.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span5.onclick = function () {
-  sharon.style.display = "none";
+  lee.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span6.onclick = function () {
-  shlok.style.display = "none";
+  long.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span7.onclick = function () {
-  zilong.style.display = "none";
+  natalie.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span8.onclick = function () {
-  lee.style.display = "none";
+  nicole.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 span9.onclick = function () {
-  kelly.style.display = "none";
+  riya.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
+};
+
+span10.onclick = function () {
+  sharon.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
+};
+
+span11.onclick = function () {
+  shlok.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
+};
+
+span12.onclick = function () {
+  zilong.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 };
 
 function close(myModal) {
   var modal = document.getElementById(myModal);
   modal.style.display = "none";
+  document.body.style.overflow = "visible";
+  document.body.style.height = "100%";
 }
